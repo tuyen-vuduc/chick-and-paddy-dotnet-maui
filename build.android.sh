d8943ae2b33c93122c7e467c509d8057ebe864e9
Tuyen Vu <hi@tuyen-vuduc.tech>
2024-05-31 06:06:29 +0700

    - another attempt

--- build.android.sh
+++ build.android.sh
@@ -2,7 +2,7 @@
 cd src/ChickAndPaddy
 dotnet publish -f net8.0-android -c Release \
     -p:AndroidKeyStore=true \
-    -p:AndroidSigningKeyStore=plantup_release.keystore \
+    -p:AndroidSigningKeyStore=x_release.keystore \
     -p:AndroidSigningKeyAlias=$KEYSTORE_ALIAS \
     -p:AndroidSigningKeyPass=$KEYSTORE_PASSWORD \
     -p:AndroidSigningStorePass=$KEYSTORE_PASSWORD
